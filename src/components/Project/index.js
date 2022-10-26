293133237817c62ffbe703b1a3be6dc6013eabab
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-10-25 22:16:43 -0400

    added json projects file

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import projects from './projects.json'
 
 function Project() {
     return (
   <div>
   <h1>Project</h1>
+  {projects.map(projects => {
+    return
+    (<div>
+      <p>{
+        projects.id}</p>
+
+      </div>  
+    )
+
+  })}
   </div>
   
     )
