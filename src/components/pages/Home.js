c34aece61e0b007fda88c70c745d8d0e4becf3d1
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-11-10 13:44:02 -0500

    Added text"

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,6 +17,7 @@ export default function Home() {
       <br></br>
       <h4>“A new idea comes suddenly and in a rather intuitive way, but intuition is nothing but the outcome of earlier intellectual experience.” - Albert Einstein</h4>
       <br></br>
+      <h4>"Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do." — Pele</h4>
       
       
         <div style={{ height: '400px' }} />
