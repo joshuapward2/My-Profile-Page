ecf32dc4c5c3d7b6c964b94c6702bb352252e830
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-12-15 16:08:17 -0500

    adding to skills and styling

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,26 +1,104 @@
 import React from 'react';
-import { Parallax } from 'react-parallax';
+// import { Parallax } from 'react-parallax';
 
 export default function About() {
   return (
-    <Parallax
-      blur={{ min: -15, max: 15 }}
-      // bgImage={require('../../assets/workspacebg.jpeg')}
-      // bgImageAlt="the dog"
-      strength={-200}
-    >
-      <section className="body">
-        <h1 className='skillsHeader'>Skills</h1>
-        <div className="my-2">
+    // <Parallax
+    //   // blur={{ min: -15, max: 15 }}
+    //   // // bgImage={require('../../assets/workspacebg.jpeg')}
+    //   // // bgImageAlt="the dog"
+    //   // strength={-200}
+    // >
+    <section className="">
+      <h1 className='skillsHeader'>Skills</h1>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="card rounded-border border-primary">
+              <h5 class="card-header">Technologies & Languages</h5>
+              <div class="card-body">
+                <ul className='card-text'>
+                  <li> HTML, CSS, JavaScript </li>
+                  <li>MERN Stack(React,Express.js, Node.js, MongoDB)</li>
+                  <li>MySQL & Sequelize</li>
+                  <li>MongoDB & Mongoose</li>
+                  <li>GraphQL & IndexDB </li>
+                  <li>Bootstrap & Bulma</li>
+                  <li>Jquery</li>
+                  <li>Google Dev Tools & React Dev Tools</li>
+                 
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
 
-          <p className='textContent'>
+
+          <div class="col-sm-4">
+            <div class="card rounded-border border-primary">
+              <h5 class="card-header">Paradigms, Concept's & Methodology's </h5>
+              <div class="card-body">
+                <ul className='card-text'>
+                  <li>Object oriented programming</li>
+                  <li>Functional programming</li>
+                  <li>Object relational mapping</li>
+                  <li>React State management</li>
+                  <li>Git version control</li>
+                  <li>Github</li>
+                  <li>Agile development methodology</li>
+                  <li>Progressive web applications</li>
+                  <li>Incorporating NPM's</li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
 
 
-            HTML, CSS and JavaScript. | Mern stack, Node, Express and React.js, MongoDB |MySQL & NoSQL, GraphQL, IndexDB databases | Bootstrap, jQuery, Bulma  | OOP and Functional programming syntaxes |ORM, Sequelize, Mongoose, several other NPM’s | Google and React developer tools | Agile development methodology | Git and GitHub workflow | State management | Data-types | Progressive Web Applications | Google and React Developer Tools
 
-          </p>
+          <div class="col-sm-4">
+            <div class="card rounded-border border-primary">
+              <h5 class="card-header">Soft Skills</h5>
+              <div class="card-body">
+                <ul className='card-text'>
+                  <li>Communication</li>
+                  <li> Team Player</li>
+                  <li>Dependable</li>
+                  <li>Fast learner</li>
+                  <li>Persistant</li>
+                  <li>Respectful</li>
+                  <li>Leader</li>
+                  <li>Rule follower</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-sm-4">
+            <div class="card rounded-border border-primary">
+              <h5 class="card-header">Skills in the making</h5>
+              <div class="card-body">
+                <ul className='card-text'>
+                  <li> AWS</li>
+                  <li> Java</li>
+                  <li>Python</li>
+                  <li>Consistent progression of data structures and algorithms</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
-    </Parallax>
+     
+    </section>
+
+
+    /* <p className='textContent'>
+
+        HTML, CSS and JavaScript. | Mern stack, Node, Express and React.js, MongoDB |MySQL & NoSQL, GraphQL, IndexDB databases | Bootstrap, jQuery, Bulma  | OOP and Functional programming syntaxes |ORM, Sequelize, Mongoose, several other NPM’s | Google and React developer tools | Agile development methodology | Git and GitHub workflow | State management | Data-types | Progressive Web Applications | Google and React Developer Tools
+
+      </p> */
+
+    /* // </Parallax> */
+
   );
 }
