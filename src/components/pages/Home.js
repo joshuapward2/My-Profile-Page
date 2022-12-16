f3c09ba768c746e7727dfb34cef6457e7c914b56
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-12-15 21:08:29 -0500

    changing home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,29 +1,35 @@
 import React from 'react';
- import {Parallax} from 'react-parallax';
+// import { Parallax } from 'react-parallax';
 
 export default function Home() {
   return (
-    
-     
-     <Parallax  
-       blur={{ min: -15, max: 15 }}
+
+
+    // <Parallax
+    //   blur={{ min: -15, max: 15 }}
       //  bgImage={require('../../assets/workspacebg.jpeg')}
       // bgImageAlt="the dog"
-      strength={-200}
-  >
-  <section className="body">
-  <h1 className="skillsHeader">About me...</h1>
-  <div className="my-2">
-    
-    <p className='textContent'>
-    Hi, I'm Josh! Im Full stack software developer and U.S military veteran. Other then software and my time in the USMC i've spent time in management, hospitality, coaching & training, customer service and sales. I also have a well-rounded understanding of the fundamentals of business and economics. I'm competent in interpreting and analyzing asset charts and graphs. I’ve earned a certificate in Full-stack Software Development from the University of Central Florida.  
-   My established communication skills and technical fundamentals, combined with a vigorous work ethic, make me a strong addition to any team. My focus is to join a team where I can apply my skills and experience, expand my knowledge and play a part in the next generation of tech. I’m excited to learn and contribute to a team’s success and help others along the way.
+    //   strength={-200}
+    // >
+      <section className="body">
+        <h1 className="skillsHeader">About me...</h1>
+        <div className="my-2">
+          <div class="col-sm-12">
+            <div class="card home rounded-border">
+              <p className='textContent'>
+                <h4>If you're curious to hear my story or just to see my resume and credentials, you're in the right spot. Please use the tabs above accordingly.</h4>
+                <br></br>
+                <span className='textIntro'> Hi, I'm Josh! </span> Im Full stack software developer, U.S military veteran and i've spent the vast majority of my professional career working in a team first enviroment. I was an athlete from elementary school through college. Then from the marine corps, nation-wide corporations to small-town staples, the teams priorities came first.  
+                Other then software and my time in the USMC i've spent time in management, hospitality, coaching & training, customer service and sales. I also have a well-rounded understanding of the fundamentals of business and economics. I'm competent in interpreting and analyzing asset charts and graphs. I’ve earned a certificate in Full-stack Software Development from the University of Central Florida.
+                My established communication skills and technical fundamentals, combined with a vigorous work ethic, make me a strong addition to any team. My focus is to join a team where I can apply my skills and experience, expand my knowledge and play a part in the next generation of tech. I’m excited to learn and contribute to a team’s success and help others along the way.
 
-    </p>
-  </div>
-  </section>
-  
-     </Parallax>
-    
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    // </Parallax>
+
   );
 }
